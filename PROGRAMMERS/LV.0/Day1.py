25cf5d55da5bee0025ab9ac2313b72a9c50f063d
HYEJI124 <pks105258@gmail.com>
2025-12-15 13:57:20 +0900

    Programmers Day1 학습

---
+++ PROGRAMMERS/LV.0/Day1.py
@@ -0,0 +1,51 @@
+# 1. 두 수의 합 구하기
+
+'''
+정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+'''
+
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
+
+print(solution(2, 3))
+print(solution(100, 2))
+
+# 2. 두 수의 차 구하기
+
+'''
+정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+'''
+
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
+
+print(solution(2, 3))
+print(solution(100, 2))
+
+# 3. 두 수의 곱 구하기
+
+'''
+정수 num1, num2가 매개변수 주어질 때, num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+'''
+
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
+
+print(solution(3, 4))
+print(solution(27, 19))
+
+# 4. 두 수의 몫 구하기
+
+'''
+정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+'''
+
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
+
+print(solution(10, 5))
+print(solution(7, 2))
